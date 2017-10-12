de86a45702c05a5b0a9a1c3c134e8921d84e94fa
山崎　亮 <yamazakiryo@yamazakisukenoMacBook-Pro.local>
2017-10-12 14:32:43 +0900

    Update users_contoroller
    -users_contorollerの仕様変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
   def edit
-
+    @user = set_user
   end
 
   def update
+    @user = set_user
+    @user.update(user_params)
     if @user.update(user_params)
-      redirect_to action: :index
+      redirect_to :root
     end
   end
 
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
