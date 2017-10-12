b89e8f0862a1b7dbbb455362f12cf5bdeb441953
山崎　亮 <yamazakiryo@yamazakisukenoMacBook-Pro.local>
2017-10-12 19:33:00 +0900

    ADD

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.update(user_params)
+      redirect_to :root
+    else
+      render :edith
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
