057a20c2462cb4ee4021678d21cb00da7920773a
山崎　亮 <yamazakiryo@yamazakisukenoMacBook-Pro.local>
2017-10-14 16:14:26 +0900

    Update groups_contoroller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,19 @@
 class GroupsController < ApplicationController
 
   def new
+   @group = Group.new
+  end
+
+  def create
   end
 
   def edit
   end
+
+  private
+
+  # def group_params
+  #   params.require(:group).permit(:name,{ :user_ids=> [] })
+  # end
 
 end
