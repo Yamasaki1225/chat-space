04c3c37bbbfa27c92efab589e9717c7af6587a63
山崎　亮 <yamazakiryo@yamazakisukenoMacBook-Pro.local>
2017-10-19 15:37:02 +0900

    Modify messages_contoroller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,15 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  def create
+    @message = Message.new(message_params)
+    if @message.save
 
   end
 
-  def create
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
