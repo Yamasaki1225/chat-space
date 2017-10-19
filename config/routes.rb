5b093036b7bbba670197adb4b5709e2df8adb722
山崎　亮 <yamazakiryo@yamazakisukenoMacBook-Pro.local>
2017-10-19 16:20:20 +0900

    Create uploaders
    アップロードする画像の大きさを設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :messages only
   resources :users
   resources :groups, only: [:new,:edit,:create,:update] do
     resources :messages, only: [:index, :create]
