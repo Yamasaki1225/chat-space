b5bb2adefbcb5d5aea85205119f7556e134900c1
山崎　亮 <yamazakiryo@yamazakisukenoMacBook-Pro.local>
2017-10-19 20:13:38 +0900

    Modify name
    自分の名前を表示できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users
-  resources :groups, only: [:new,:edit,:create,:update] do
+  resources :users, only: [:show, :edit, :update]
+  resources :groups, only: [:new, :edit, :create, :update] do
     resources :messages, only: [:index, :create]
   end
 end
