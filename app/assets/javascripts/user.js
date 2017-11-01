b0096f01b62a700234865493b42fdfa71b1d5863
山崎　亮 <yamazakiryo@yamazakisukenoMacBook-Pro.local>
2017-11-01 20:49:57 +0900

    Modify

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,7 +3,6 @@ $(function(){
   var user_list = $("#user-member-list");
 
   function appendUserList(user){
-    console.log(result)
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}" id="addbutton" >追加</a>
@@ -21,12 +20,6 @@ $(function(){
     user_list.append(html);
   }
 
-  // function appendNoUser(user) {
-  //   var html = `<div class="chat-group-user clearfix">
-  //                 <p class="chat-group-user__name"></p>
-  //                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="" data-user-name="" id="button" >追加</a>
-  //                 </div>`
-  // }
 
   $('#user-search-field').on('keyup', function(e){
     e.preventDefault();
@@ -40,30 +33,22 @@ $(function(){
       })
 
     .done(function(users){
-      console.table(users);
       $('#user-search-result').empty();
       if (users.length !== 0 ) {
         users.forEach(function(user){
           appendUserList(user);
         });
        }
-      // else {
-      //   appendNoUser("一致するユーザーがいません");
-      // }
      })
       .fail(function(){
         alert('ユーザー検索に失敗しました。');
       })
     });
 
-
   $(document).on("click", '.chat-group-user__btn--add', function(){
     $(this).parent().remove();
     var id   = $(this).attr('data-user-id');
     var name = $(this).attr('data-user-name');
-    console.log(id);
-    console.log(name);
-
     appendMember(id, name);
   })
   $(document).on("click", '.js-remove-btn', function(){
